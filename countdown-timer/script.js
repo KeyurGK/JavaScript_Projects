49df15410b015a272970e16635a0757befe2b0e0
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2022-12-19 10:13:29 +0530

    commit

---
+++ countdown-timer/script.js
@@ -0,0 +1,22 @@
+const daysEl=document.getElementById('days');
+const hoursEl=document.getElementById('hours');
+const minsEl=document.getElementById('mins');
+const secsEl=document.getElementById('seconds');
+const newYears='1 Jan 2023';
+function countDown(){
+    const currentDate=new Date();
+    const newYearsDate=new Date(newYears);
+    const seconds=(newYearsDate-currentDate)/1000;
+    const days=Math.floor(seconds/3600/24);
+    const hours=Math.floor(seconds/3600)%24;
+    const minutes=Math.floor(seconds/60)%60;
+    const secs=Math.floor(seconds)%60;
+
+    console.log(days,hours,minutes,secs);
+    daysEl.innerHTML=days;
+    hoursEl.innerHTML=hours;
+    minsEl.innerHTML=minutes;
+    secsEl.innerHTML=secs;
+}
+countDown();
+setInterval(countDown,1000);
