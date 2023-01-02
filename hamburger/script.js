941006fa1ef8577bf466c32c2c77ad273c87e524
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-01-02 12:01:55 +0530

    commit

--- hamburger/script.js
+++ hamburger/script.js
@@ -1,13 +1,10 @@
 const btn=document.getElementById('btn');
 const nav=document.getElementById('nav');
-const list=document.getElementById('list');
+
 
 btn.addEventListener('click', ()=>{
     nav.classList.add('active');
     btn.classList.toggle('active');
-    const content='heyhey';
-    const liel=document.createElement('li');
-    liel.innerText=content;
-    list.appendChild(liel);
+   
     
 });
