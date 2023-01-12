47a6b42187e3f0b7957eff0052681fa0c8316341
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-01-12 10:10:29 +0530

    commit

--- purple-heart-rain/script.js
+++ purple-heart-rain/script.js
@@ -1,6 +1,6 @@
 function createHeart(){
     const heart=document.createElement('div');
-    heart.innerText='❤️';
+    heart.innerText='😍😍';
     heart.classList.add('heart');
     heart.style.left = Math.random()*100 +'vw';
     heart.style.animationDuration=Math.random()*2+1+'s';
