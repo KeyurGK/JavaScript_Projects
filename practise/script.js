eeadeaf81b7f6328b56d664aeecb15d2e9f58457
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-01-28 14:23:26 +0530

    commit

--- practise/script.js
+++ practise/script.js
@@ -1,10 +1,14 @@
-const cover = document.querySelector('.cover');
-function snowing(){
-    const data = document.createElement('div');
-    data.classList.add('data');
-    data.innerHTML = '❄️';
-    data.style.left=(Math.random()*100)+'vw';
-    cover.appendChild(data);
+const changeContentEl=document.getElementById('change-content');
+function load(){
+    load1();
+    
+ }
+function load1(){
+ changeContentEl.innerHTML='a';
+ setTimeout(load2, 500); 
 }
-
-setInterval(snowing,500);
+function load2(){
+    changeContentEl.innerHTML='b';
+    
+}
+setInterval(load1,3000);
