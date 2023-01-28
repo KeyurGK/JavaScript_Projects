b718bcca73599657474a005a39d5f563f3470f6b
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-01-28 14:27:45 +0530

    commit

--- background-changer/script.js
+++ background-changer/script.js
@@ -5,5 +5,8 @@ btn.addEventListener('click',()=>{
 });
 
 function randomBg(){
-    return `rgb(${Math.floor(Math.random()*360)}, 100, 20)`;
+    return `rgb(${Math.floor(Math.random()*360)}, 20, 120)`;
 }
+btn.style.color='white';
+btn.style.padding='1.5rem';
+btn.style.borderRadius='2px';
