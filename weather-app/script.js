b9b1d2470e898cf5341dd0dbcf2f96bb419de1ae
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-02-01 10:18:15 +0530

    commit

--- weather-app/script.js
+++ weather-app/script.js
@@ -2,7 +2,9 @@ const contentEl=document.getElementById('content');
 const searchEl=document.getElementById('search');
 const searchBtn=document.getElementById('search-button');
 const locationEl=document.getElementById('location');
-const headerEl=document.getElementById('header');
+var col1El=document.getElementById('col-1').querySelectorAll('.col-1');
+
+
 
 const APIURL = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=';
 const options = {
@@ -13,73 +15,34 @@ const options = {
 	}
 };
 
-headerEl.innerHTML=`
-        <h1 class='title'>Weather App</h1>
-        <div class="search-bar">
-             <input type="text" id="search" placeholder="Enter the location" autocomplete="off"/>
-             <button id="search-button" class="search-button">Search</button>
-        </div>`;
 
-// searchBtn.addEventListener('click',(e)=>
-// {
-//     e.preventDefault();
-//     let location = searchEl.value;
-//     if(location){
-//         getWeather(location);
-//     }
-//     else{
-//         alert('Please enter the location!');
-//     }
-// });
-// getWeather('Dharwad');
-// async function getWeather(location){
-//     const resp=await fetch(APIURL+location,options);
-//     const respData=await resp.json();
-//     addLocation(respData,location);
-//     console.log(respData);
-// }
+searchBtn.addEventListener('click',(e)=>
+{
+    e.preventDefault();
+    let location = searchEl.value;
+    if(location){
+        getWeather(location);
+    }
+    else{
+        alert('Please enter the location!');
+    }
+});
 
-// function addLocation(locationData,location){
-//     contentHTML = `
-//      <div class="box-1">
-//         <div class="humidity">
-//             <p>
-//                 Humidity<br><br>
-//                 ${locationData.humidity}
-//             </p>
-//         </div>    
-//         <div class="min-temp">
-//             <p>
-//             Minimum Temperature<br><br>
-//                 ${locationData.min_temp} °c
-//             </p>
-//         </div>
-//         <div class="temp">
-//             <p>
-//             Temperature<br><br>
-//                 ${locationData.temp} °c
-//             </p>
-//         </div>
-//         <div class="max-temp">
-//             <p>
-//              Maximum Temperatue<br><br>
-//                 ${locationData.max_temp} °c
-//             </p>
-//         </div>
-//         <div class="wind-speed">
-//             <p>
-//             Wind Speed<br><br>
-//                 ${locationData.wind_speed} knots
-//             </p>
-//         </div>
-//         </div>
-//     <div class='box-2'>
-//         <div class='location'>
-//             <p>${location}</p>
-//         </div>
-//     </div>`
-//     contentEl.innerHTML = contentHTML;
-// }
+async function getWeather(location){
+    const resp=await fetch(APIURL+location,options);
+    const respData=await resp.json();
+    addLocation(respData,location);
+    console.log(respData);
+};
+
+function addLocation(locationData,location){
+        // col1El[0].innerHTML= '🔥';
+         col1El[1].innerHTML= `${locationData.temp}`;
+}
+
+
+
+
 
 
 // searchBtn.style.padding = '5px 20px';
