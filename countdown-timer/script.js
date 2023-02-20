43ce460eea6dc07a70c91f63afd58273bcd7b260
Keyur G Kulkarni <56074740+KeyurGK@users.noreply.github.com>
2023-02-20 17:02:56 +0530

    Add files via upload

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -2,21 +2,29 @@ const daysEl=document.getElementById('days');
 const hoursEl=document.getElementById('hours');
 const minsEl=document.getElementById('mins');
 const secsEl=document.getElementById('seconds');
-const newYears='1 Jan 2023';
+const submitBtn = document.getElementById('submit');
+var inputDateEl = document.getElementById('input-date');
+let targetYear
+
+submitBtn.addEventListener('click',() =>
+{
+    targetYear=inputDateEl.value
+    countDown();
+    setInterval(countDown,1000)
+})
 function countDown(){
-    const currentDate=new Date();
-    const newYearsDate=new Date(newYears);
-    const seconds=(newYearsDate-currentDate)/1000;
+    const currentDate=new Date();    
+    const targetYearsDate=new Date(targetYear);
+    const seconds=(targetYearsDate-currentDate)/1000;
     const days=Math.floor(seconds/3600/24);
     const hours=Math.floor(seconds/3600)%24;
     const minutes=Math.floor(seconds/60)%60;
     const secs=Math.floor(seconds)%60;
 
-    console.log(days,hours,minutes,secs);
+    
     daysEl.innerText=days;
     hoursEl.innerHTML=hours;
     minsEl.innerHTML=minutes;
     secsEl.innerText=secs;
 }
-countDown();
-setInterval(countDown,1000);
+
