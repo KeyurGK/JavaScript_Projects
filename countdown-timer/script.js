854d5da5f546381a8d6e0af6fedc7509c280b866
Keyur G Kulkarni <56074740+KeyurGK@users.noreply.github.com>
2023-02-21 12:22:49 +0530

    Add files via upload

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -3,19 +3,28 @@ const hoursEl=document.getElementById('hours');
 const minsEl=document.getElementById('mins');
 const secsEl=document.getElementById('seconds');
 const submitBtn = document.getElementById('submit');
-var inputDateEl = document.getElementById('input-date');
-let targetYear
+var startDateEl = document.getElementById('start-date');
+var targetDateEl = document.getElementById('target-date');
+var startTimeEl = document.getElementById('start-time');
+var targetTimeEl = document.getElementById('target-time');
+let targetDate,startDate,startTime,targetTime
 
 submitBtn.addEventListener('click',() =>
 {
-    targetYear=inputDateEl.value
+    startDate = startDateEl.value
+    startTime = startTimeEl.value
+    targetDate = targetDateEl.value
+    targetTime = targetTimeEl.value
+    console.log(startTime)
     countDown();
     setInterval(countDown,1000)
 })
 function countDown(){
-    const currentDate=new Date();    
-    const targetYearsDate=new Date(targetYear);
-    const seconds=(targetYearsDate-currentDate)/1000;
+    const startYearsDate = new Date(`${startDate},${startTime}`)
+    const targetYearsDate=new Date(`${targetDate},${targetTime}`);
+    
+
+    const seconds=(targetYearsDate-startYearsDate)/1000;
     const days=Math.floor(seconds/3600/24);
     const hours=Math.floor(seconds/3600)%24;
     const minutes=Math.floor(seconds/60)%60;
